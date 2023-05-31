2c0cf0af5b37765cb0a51c0282dfc00e6b255994
Divyansh0108 <divyanshpandey0108@gmail.com>
2023-05-31 19:33:56 +0530

    split function technique for reversal of string but not words

---
+++ reverse1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class reverse1 {
+    public static void main(String[] args) {
+        Scanner xp = new Scanner(System.in);
+        System.out.println("Enter the sentence:- ");
+        String s = xp.nextLine();
+        String s1 = "";
+        String s2[] = s.split(" ");
+        for(int i=s2.length-1;i>=0;i--)
+        {
+            s1 = s1 + " " + s2[i];
+        }
+        System.out.println("the reversed string is:- " +s1);
+        xp.close();
+    }
+}
